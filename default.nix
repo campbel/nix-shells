9f84f148b465771ab2ab1e4cbde0050457f35c61
Chris Campbell <chrs.campbell@shopify.com>
2023-03-31 06:54:33 -0600

    rev++

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {};
 let
     dotfiles=pkgs.fetchgit {
         url = "https://github.com/campbel/dotfiles.git";
-        rev = "b40e8413346e5c2b18a64daa08aeda317b35bc78";
+        rev = "e84a313e0d006ca38e83e21cab1bca612c4b6b01";
         sha256 = "i2otjmnIscnkAjWcI084nkhwsJLnPISQzUPaWv2VqKc=";
     };
 in
